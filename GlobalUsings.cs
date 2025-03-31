2e615d707cf403042855e0ff941fbdfdf7269b72
haniyehasadi <haniyeasadi627@gmail.com>
2025-03-31 23:56:06 +0330

    Add Global Usings

---
+++ GlobalUsings.cs
@@ -0,0 +1,10 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata;
+global using Microsoft.Data.SqlClient;
+global using System.ComponentModel.DataAnnotations;
+global using MoheymanProject.Data;
+global using MoheymanProject.Services;
+global using MoheymanProject.Models;
